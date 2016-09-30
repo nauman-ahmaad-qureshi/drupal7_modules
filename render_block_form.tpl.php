6893de2b185878235d6684c934049985c3c8391e
Nauman Ahmed <Nauman@datum.lahore>
2016-09-30 20:50:50 +0500

    render block module

---
+++ render_block_form.tpl.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @file
+ * Default template for render block form.
+ *
+ * Available variables:
+ * - $form: the render block form 
+ */
+?>
+<?php print drupal_render($form); ?>
---
+++ rendered_block_results.tpl.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @file
+ * Default template for render block form results
+ *
+ * Available variables:
+ * - $results: the render block form results 
+ */
+?>
+<?php print $results; ?>
